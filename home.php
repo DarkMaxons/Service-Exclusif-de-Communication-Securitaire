ccd9aebdb360471e5ee30c00a5fe7d54b7567faf
DomCriouco <40230973+DomCriouco@users.noreply.github.com>
2019-04-22 19:53:37 +0200

    version 0.0.1

---
+++ home.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Home</title>
+</head>
+<body>
+	<header>
+		<div id="entête">
+
+			<div class="segment">
+				<div id="entête1">
+					<div class="bouton_entête1">
+						<a href="inscrit.php">
+							nombres d'inscrit 
+						</a>
+					</div>
+					<div class="bouton_entête1">
+						<a href="signup.php">
+						 	sign up
+						</a>
+						|
+						<a href="signin.php">
+						 	sign in 
+						</a>
+					</div>
+				</div>	
+			</div>
+
+			<div class="segment">
+				bonjour
+			</div>
+
+			<div class="segment">
+				<div id="entête2">
+
+					<div class="bouton_entête2">
+						<a href="home.php">
+						 	Home. 
+						</a>
+					</div>
+
+					<div class="bouton_entête2">
+						<a href="coms.php">
+						 	Coms.
+						</a>
+					</div>
+
+					<div class="bouton_entête2">
+						<a href="staff.php">
+						 	Staff.
+						</a>
+					</div>
+
+					<div class="bouton_entête2">
+						<a href="param.php">
+						 	Param.
+						</a>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+	</header>
+</body>
+</html>
